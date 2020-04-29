059ab38b4f8fc152e4484b5c25c3c12c72fddc67
Adam <Anoah1225@gmail.com>
2020-04-29 10:49:48 -0400

    revised some things

--- src/components/footer.js
+++ src/components/footer.js
@@ -30,8 +30,8 @@ const Footer = () => {
               </Col>
               <Col className={footerStyles.footerNav}>
                 <h5>CONTACT US</h5>
-                <p>TOLL FREE: <a href="tel:954-826-4318">(800)-888-8888</a></p>
-                <p>EMAIL:<a href="info@betterwellnessusa.com"> info@betterwellnessusa.com</a></p>
+                <p>TOLL FREE: <a href="tel:954-826-4318">(954)-533-4245</a></p>
+                <p>EMAIL:<a href="info@betterwellnessusa.com"> contact@goldengoatusa.com</a></p>
                </Col>
                <Col className={footerStyles.footerSocial}>
                <a href="https://www.facebook.com/GoldenGoatUSA/" target="_blank" rel="noopener noreferrer"><FacebookIcon color="primary" fontSize="medium"/></a>
