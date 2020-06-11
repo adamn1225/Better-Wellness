6aeb9a4fbb91493138e6acd8a5b1a919f313eb93
adamn1225 <anoah1225@gmail.com>
2020-06-11 16:58:16 -0400

    analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
           gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
           gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
           dataLayerName: "YOUR_DATA_LAYER_NAME",
-          routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+          routeChangeEventName: "gatsby-route-change",
         },
       },
       'gatsby-plugin-sass',
